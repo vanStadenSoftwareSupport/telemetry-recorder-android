2079662a2787a89f98645a67d6d6b42abeb4b562
Grant van Staden <grantv@youi.email>
2017-12-08 12:26:04 +1000

    Added test for saving start state

--- telemetry_recorder/telemetryLib/src/test/java/edu/self/telemetry_recorder/InMemoryTelemetryDataStoreTester.java
+++ telemetry_recorder/telemetryLib/src/test/java/edu/self/telemetry_recorder/InMemoryTelemetryDataStoreTester.java
@@ -3,7 +3,9 @@ package edu.self.telemetry_recorder;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * @author grantvanstaden
@@ -26,5 +28,20 @@ public class InMemoryTelemetryDataStoreTester {
         TelemetryDataStore dataStore = new InMemoryTelemetryDataStore();
         String actual = dataStore.getRegistration("BOGUS");
         assertNull(actual);
+    }
+
+    @Test
+    public void canGetStartState() {
+        TelemetryDataStore dataStore = new InMemoryTelemetryDataStore();
+        boolean actual = dataStore.isStartedStateRecorded();
+        assertFalse(actual);
+    }
+
+    @Test
+    public void canSaveStartState() {
+        TelemetryDataStore dataStore = new InMemoryTelemetryDataStore();
+        dataStore.recordStartState(true);
+        boolean actual = dataStore.isStartedStateRecorded();
+        assertTrue(actual);
     }
 }
