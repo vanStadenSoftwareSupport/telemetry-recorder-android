80c961b3b758369e63ab72f8e68c05142de07e6c
grantvanstaden <grant.vanstaden@live.com.au>
2017-12-11 09:02:22 +1000

    Made TelemetryRecorderManager an internal interface.

--- telemetry_recorder/telemetryLib/src/main/java/edu/self/telemetry_recorder/TelemetryRecorderManager.java
+++ telemetry_recorder/telemetryLib/src/main/java/edu/self/telemetry_recorder/TelemetryRecorderManager.java
@@ -4,10 +4,10 @@ package edu.self.telemetry_recorder;
  * Created by grantvanstaden on 2017/12/07.
  */
 
-public interface TelemetryRecorderManager {
-    public String getRegistration(String userHostAppId) ;
-    public String register(String userHostAppId) ;
-    public boolean isStarted() ;
-    public void start() ;
-    public void stop() ;
+ interface TelemetryRecorderManager {
+    String getRegistration(String userHostAppId) ;
+    String register(String userHostAppId) ;
+    boolean isStarted() ;
+    void start() ;
+    void stop() ;
 }
